83dc75875c6208f980c1c1bfaa3996795620bf22
KamilSzus <kamil-szustak1@wp.pl>
2022-03-06 20:59:16 +0100

    add layout

--- app/src/main/java/com/example/kalkulator/MainActivity.java
+++ app/src/main/java/com/example/kalkulator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kalkulator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clearOnClick(View view) {
+    }
+
+    public void powerOnClick(View view) {
+    }
+
+    public void multiplyOnClick(View view) {
+    }
+
+    public void divisionOnClick(View view) {
     }
 }
